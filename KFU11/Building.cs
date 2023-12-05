9b6cf6fa03b9d2cbddb00f9a09a5e0cc16cb9b0f
kaneki44 <nuzhinvlad98@gmail.com>
2023-12-05 23:12:42 +0300

    dfgh

---
+++ KFU11/Building.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace KFU11
+{
+	public class Building
+	{
+		public Building()
+		{
+		}
+	}
+}
+
---
+++ Lab14/RationalNumber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Lab14
+{
+	public class RationalNumber
+	{
+		public RationalNumber()
+		{
+		}
+	}
+}
+
